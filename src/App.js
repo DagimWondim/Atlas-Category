1318ddbb7c48453b9d1e8defd757e36bd4a2983a
Your Name <dagimwondim25@gmail.com>
2023-10-08 23:57:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,171 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Keyboard, Scrollbar, Navigation, Pagination } from 'swiper/modules';
+
+
+import './App.css';
+import car from './chr.jpg'
+import mar from './marcedes.webp'
+import chr from './tucson.png'
+import tuc from './tucson.jpg'
+// import required modules
+
+import Header from './component/Header';
+import Main from './component/Main';
+import Categ from './component/Categ';
+import Footer from './component/Footer';
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Header />
+    <Main 
+      quant="4"
+      categ="Car"
+    />
+    <>
+      <Swiper
+        slidesPerView={3}
+        centeredSlides={false}
+        slidesPerGroupSkip={3}
+        grabCursor={true}
+        keyboard={{
+          enabled: true,
+        }}
+        breakpoints={{
+          769: {
+            slidesPerView: 3,
+            slidesPerGroup: 2,
+          },
+        }}
+        scrollbar={true}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+        className="mySwiper"
+      >
+      <div  id="sec">
+        <SwiperSlide>
+        <Categ  
+       profile=" Dagi.W"
+       image={car}
+       model="Tucson"
+       price="$200"
+       location="Harer"
+    />
+        </SwiperSlide>
+        <SwiperSlide>
+        <Categ 
+       profile=" Dagi.W"
+       image={tuc}
+       model="Tucson"
+       price="$200"
+       location="Harer"
+    />
+        </SwiperSlide>
+        <SwiperSlide>
+        <Categ 
+       profile=" Dagi.W"
+       image={mar}
+       model="Tucson"
+       price="$200"
+       location="Harer"
+    />
+        </SwiperSlide>
+        <SwiperSlide>
+        <Categ  
+       profile=" Fikir.E"
+       image={car}
+       model="Tucson"
+       price="$200"
+       location="Harer"
+    />
+        </SwiperSlide>
+        <SwiperSlide>
+        <Categ 
+       profile=" Dagi.W"
+       image={chr}
+       model="Tucson"
+       price="$200"
+       location="Harer"
+    />
+        </SwiperSlide>
+        <SwiperSlide>
+        <Categ 
+       profile=" Dagi.W"
+       image={mar}
+       model="Tucson"
+       price="$200"
+       location="Harer"
+    />
+        </SwiperSlide>
+        <SwiperSlide>
+        <Categ  
+       profile=" Dagi.W"
+       image={car}
+       model="Tucson"
+       price="$200"
+       location="Harer"
+    />
+        </SwiperSlide>
+        <SwiperSlide>
+        <Categ 
+       profile=" Dagi.W"
+       image={tuc}
+       model="Tucson"
+       price="$200"
+       location="Harer"
+    />
+        </SwiperSlide>
+        <SwiperSlide>
+        <Categ 
+       profile=" Dagi.W"
+       image={mar}
+       model="Tucson"
+       price="$200"
+       location="Harer"
+    />
+        </SwiperSlide>
+        <SwiperSlide>
+        <Categ  
+       profile=" Dagi.W"
+       image={car}
+       model="Tucson"
+       price="$200"
+       location="Harer"
+    />
+        </SwiperSlide>
+        <SwiperSlide>
+        <Categ 
+       profile=" Dagi.W"
+       image={chr}
+       model="Tucson"
+       price="$200"
+       location="Harer"
+    />
+        </SwiperSlide>
+        <SwiperSlide>
+        <Categ 
+       profile=" Dagi.W"
+       image={mar}
+       model="Tucson"
+       price="$200"
+       location="Harer"
+    />
+        </SwiperSlide>
+        </div>
+      </Swiper>
+    </>
+    <Footer />
     </div>
   );
 }
-
-export default App;
